b42ebed46b959f99ec695c5d7d3cdc246ad042d5
ivladyka <ivladyka@github.com>
2021-09-02 18:16:56 +0300

    minor fix

--- src/main/java/com/ivladyka/reqappnotificationservice/controller/EmailController.java
+++ src/main/java/com/ivladyka/reqappnotificationservice/controller/EmailController.java
@@ -2,6 +2,7 @@ package com.ivladyka.reqappnotificationservice.controller;
 
 
 import com.ivladyka.reqappnotificationservice.service.EmailService;
+import com.ivladyka.reqappnotificationservice.util.QueueConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailController {
 
     private final EmailService emailService;
+    private final QueueConsumer queueConsumer;
+
 
     @Autowired
-    public EmailController(EmailService emailService) {
+    public EmailController(EmailService emailService, QueueConsumer queueConsumer) {
+        this.queueConsumer = queueConsumer;
         this.emailService = emailService;
     }
+
     @RequestMapping("/email")
     public void sendEmail(@RequestBody String email) {
-        emailService.sendEmail();
+
+        emailService.sendEmail(email);
     }
 }
--- src/main/java/com/ivladyka/reqappnotificationservice/util/QueueConsumer.java
+++ src/main/java/com/ivladyka/reqappnotificationservice/util/QueueConsumer.java
@@ -6,7 +6,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ivladyka.reqappnotificationservice.model.NotificationRequestTemplate;
 import com.ivladyka.reqappnotificationservice.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class QueueConsumer {
 
     @Autowired
